ca7e8f0103ff7cd7ecbde5700f2b01f45cdb5d63
Andrea Robles <androb.l1999@gmail.com>
2023-02-06 18:30:50 -0600

    Se agrego el template de create.html de un producto, y se hizo el enlace con la plantilla que muestra los productos.

--- demo/demo/src/main/java/com/curso/ecommerce/demo/controller/ProductoController.java
+++ demo/demo/src/main/java/com/curso/ecommerce/demo/controller/ProductoController.java
@@ -11,6 +11,11 @@ public class ProductoController {
 	public String show() {
 		return "productos/show";
 	}
+	@GetMapping("/create")
+	public String create(){
+		return "productos/create";
+		
+	}
 	
 	
 }
