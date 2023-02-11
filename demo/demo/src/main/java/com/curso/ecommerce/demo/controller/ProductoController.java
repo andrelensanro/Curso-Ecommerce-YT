38d761435f7dbb0c266c0f76214f5f8e8bf28fa2
Andrea Robles <androb.l1999@gmail.com>
2023-02-10 22:19:43 -0600

    se agrego las funcionalidades para cargar la imagen

--- demo/demo/src/main/java/com/curso/ecommerce/demo/controller/ProductoController.java
+++ demo/demo/src/main/java/com/curso/ecommerce/demo/controller/ProductoController.java
@@ -1,5 +1,6 @@
 package com.curso.ecommerce.demo.controller;
 
+import java.io.IOException;
 import java.util.Optional;
 
 import org.slf4j.*;
@@ -10,9 +11,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
 import com.curso.ecommerce.demo.model.Producto;
 import com.curso.ecommerce.demo.model.Usuario;
 import com.curso.ecommerce.demo.service.ProductoService;
+import com.curso.ecommerce.demo.service.UploadFileService;
 
 @Controller
 @RequestMapping("/productos")
@@ -22,6 +27,9 @@ public class ProductoController {
 	
 	@Autowired
 	private  ProductoService productoService;
+	@Autowired
+	private UploadFileService uploadFileService;
+	
 	
 	@GetMapping("")
 	public String show(Model model) {
@@ -34,9 +42,16 @@ public class ProductoController {
 		
 	}
 	@PostMapping("/save")
-	public String save(Producto producto) {	
+	public String save(Producto producto, @RequestParam("img") MultipartFile file) throws IOException {	
 		Usuario usuario = new Usuario(1, "", "", "", "", "", "", "" );
 		producto.setUsuario(usuario);
+		
+		//manejo de la imagen	
+		// caso: el producto que se guarda es su primera vez
+		String nameImage = uploadFileService.saveImage(file);
+		producto.setImagen(nameImage);
+		
+		
 		productoService.save(producto);
 		LOGGER.info("Este es el objeto producto {}", producto);
 		return "redirect:/productos";
@@ -53,13 +68,43 @@ public class ProductoController {
 	}
 	
 	@PostMapping("/update")
-	public String update(Producto producto) {
+	public String update(Producto producto, @RequestParam("img") MultipartFile file) throws IOException {
+		
+		/*
+		 * EL PRODUCTO QUE SE OBTIENE COMO PARAMETRO TIENE TODOS LOS CAMPOS MENOS LA IMAGEN
+		 * 
+		 * siempre tiene una imagen "default.jpg"
+		 * EL UPDATE PUEDE HACERSE primero no tiene imagen ("default.jpg") y despues si tiene una imagen
+		 * 						   primero tiene y se queda con la imagen que tiene
+		 *                         
+		 * 
+		 * */
+		Producto p = new Producto();
+		p = productoService.get(producto.getId()).get();
+		if(file.isEmpty()) {
+			producto.setImagen(p.getImagen());
+		}else {
+			if(!p.getImagen().equals("default.png")){
+				uploadFileService.deleteImage(p.getImagen());
+			}
+			String nameImage = uploadFileService.saveImage(file);
+			producto.setImagen(nameImage);
+		}
 		productoService.update(producto);
 		return "redirect:/productos";
 	}
 	
 	@GetMapping("/delete/{id}")
 	public String delete(@PathVariable Integer id) {
+		
+		Producto p = new Producto();
+		p = productoService.get(id).get();
+		
+		if(!p.getImagen().equals("default.jpg")) {
+			uploadFileService.deleteImage(p.getImagen());
+		}
+		
+		
 		productoService.delete(id);
 		return "redirect:/productos";
 	}
