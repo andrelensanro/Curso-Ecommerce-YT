f7d6af0d7e58968d0e5cee05277978df7de5ef2f
Andrea Robles <androb.l1999@gmail.com>
2023-02-15 00:15:54 -0600

    Se agrego la funcionalidad de ver los detalles del producto.

--- demo/demo/src/main/java/com/curso/ecommerce/demo/controller/HomeController.java
+++ demo/demo/src/main/java/com/curso/ecommerce/demo/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.curso.ecommerce.demo.controller;
 
+import java.util.Optional;
+
 import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.curso.ecommerce.demo.model.Producto;
 import com.curso.ecommerce.demo.service.ProductoService;
 
 @Controller
@@ -28,8 +31,15 @@ public class HomeController {
 	}
 	
 	@GetMapping("productohome/{id}")
-	public String productoHome(@PathVariable Integer id) {
+	public String productoHome(@PathVariable Integer id, Model model) {
 		log.info("Id producto enviado como parametro {}", id);
+		
+		Producto producto = new Producto();
+		Optional<Producto> productoOptional = productoService.get(id);
+		producto = productoOptional.get();
+		
+		model.addAttribute("producto",producto );
+		
 		return "usuario/productohome";
 	}
 	
