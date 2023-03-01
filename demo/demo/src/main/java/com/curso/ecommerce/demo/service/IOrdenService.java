bc321e4a2467fef57b1d05bec32f00048bb2f6f7
Andrea Robles <androb.l1999@gmail.com>
2023-03-01 13:07:54 -0600

    Se agrego el metodo para manejar el numero de orden a tratar.

--- demo/demo/src/main/java/com/curso/ecommerce/demo/service/IOrdenService.java
+++ demo/demo/src/main/java/com/curso/ecommerce/demo/service/IOrdenService.java
@@ -1,7 +1,10 @@
 package com.curso.ecommerce.demo.service;
 
+import java.util.List;
+
 import com.curso.ecommerce.demo.model.Orden;
 
 public interface IOrdenService {
 	Orden save (Orden orden);
+	List<Orden> findAll();
 }
--- demo/demo/src/main/java/com/curso/ecommerce/demo/service/OrdenServiceImpl.java
+++ demo/demo/src/main/java/com/curso/ecommerce/demo/service/OrdenServiceImpl.java
@@ -1,5 +1,8 @@
 package com.curso.ecommerce.demo.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +20,33 @@ public class OrdenServiceImpl implements IOrdenService {
 	public Orden save(Orden orden) {
 		
 		return ordenRepository.save(orden);
+	}
+	@Override
+	public List<Orden> findAll() {
+		return ordenRepository.findAll();
+	}
+	
+	public String generateNumberOrden() {
+		int n = 0;
+		List<Orden> ordenes = findAll();
+		List<Integer> numeros = new ArrayList<Integer>();
+		ordenes.stream().forEach(ord -> numeros.add(Integer.parseInt(ord.getNumero())));
+		
+		if(ordenes.isEmpty()) {
+			n = 1;
+		}else {
+			n = numeros.stream().max(Integer::compare).get();
+			n++;
+		}
+		String nAsString = String.valueOf(n); 
+				
+		String resp = "";
+		for(int i=0; i<10-nAsString.length(); i++) {
+			resp+="0";
+		}
+	
+		
+		return resp + nAsString;
 	}
 
 }
